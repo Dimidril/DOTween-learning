01de944d18188b2e6a3420c5133e6b2138adc29b
Dmitry <Dimidril@users.noreply.github.com>
2021-01-16 17:42:11 +0300

    Initial commit

---
+++ Assets/Scripts/SimpleMovement/MoveTo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveTo : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOMove(new Vector3(3, 5, 1), 3).From().SetLoops(-1, LoopType.Yoyo);
+    }
+}
